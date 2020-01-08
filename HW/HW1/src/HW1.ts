0a3b6d6b13d3285b5c515a56f1bbf981fbfea32c
Vladimir <vovamazur2004@gmail.com>
2020-01-08 20:02:28 +0300

    HW1

--- HW/HW1/src/HW1.ts
+++ HW/HW1/src/HW1.ts
@@ -1,49 +1,37 @@
 //Задача 1
-function isPalindrome(str: string): boolean {
-    if (!str)
-        return false;
-    for (let i = 0; i < str.length / 2; i++) {
-        if (str.charAt(i) !== str.charAt(str.length - i - 1))
-            return false;
+function isPalindrom(word:string):string {
+    let reverseWord:string = word.split("").reverse().join("")
+    if (reverseWord === word) {
+        return true
+    } else {
+        return false
     }
-    return true;
 }
 
-console.log('sasdf');
-console.log(isPalindrome('afdadsf'));
-console.log(isPalindrome('asdffdsa'));
+console.log (isPalindrom('asfadaf'))
+console.log(isPalindrom('asdffdsa'));
 
 //Задача 2
-function fibonachiArray(fibonachiNumber: number): Array<number> {
-    let fibonachi: Array<number> = [];
-        if (fibonachiNumber === 1) {
-            fibonachi = [1]
-            return fibonachi
-        } 
-    if (fibonachiNumber <= 0) return fibonachi;
-    for (;;) {
-        let {length} = fibonachi;
-        if (length < 2){
-            fibonachi.push(1);
-            continue;
-        }
-        
-        let nextValue = fibonachi[length - 1] + fibonachi[length - 2];
-        if (length === fibonachiNumber){
-            break;
-        } 
-        fibonachi.push(nextValue);
+function getFibonachiArray(num:number):Array {
+    let resultArray:array = [];
+    if (num > 0) {
+        resultArray.push(1)
     }
-    return fibonachi;
+    if (num > 1) {
+        resultArray.push(1)
+    }
+    if (num > 2) {
+        for (let i:number = 2; i < num; i++) {
+            let newNum:number = resultArray[i-1] + resultArray[i-2]
+            if (newNum <= num){
+                resultArray.push(newNum)
+            }
+        }
+    }
+    return resultArray
 }
-console.log(fibonachiArray(0));
-console.log(fibonachiArray(1));
-console.log(fibonachiArray(2));
-console.log(fibonachiArray(3));
-console.log(fibonachiArray(4));
-console.log(fibonachiArray(5));
-console.log(fibonachiArray(6));
-console.log(fibonachiArray(7));
-console.log(fibonachiArray(8));
-console.log(fibonachiArray(9));
-console.log(fibonachiArray(10));
+
+console.log(getFibonachiArray(0));
+console.log(getFibonachiArray(1));
+console.log(getFibonachiArray(2));
+console.log(getFibonachiArray(10));
